1343072ea16818a92eddec8787f070355eff53d8
antoineaudrain <antoine.audrain@outlook.fr>
2020-10-29 15:22:44 +0100

    Landing contact responsive

--- components/organisms/LandingContact.js
+++ components/organisms/LandingContact.js
@@ -54,67 +54,83 @@ export default function LandingContact({ fullName, setFullName, email, setEmail,
     <section className={styles.landingContact}>
 
       <div className={styles.landingContact__overlay}>
-        <h1 className={styles.landingContact__overlay__title}>
-          Nos partenaires
-        </h1>
+        <div className={styles.landingContact__overlay__inner}>
+          <h1 className={styles.landingContact__overlay__title}>
+            Nos partenaires
+          </h1>
 
-        <Swiper
-          loop
-          slidesPerView={4}
-          spaceBetween={32}
-          style={{ margin: '92px 0', width: '100%' }}
-        >
-          {sponsors.map((sponsor, index) => (
-            <SwiperSlide key={index}>
-              <div key={index} className={styles.landingContact__swiperItem}>
-                <img alt={sponsor.alt} src={sponsor.asset} className={styles.landingContact__swiperItem__image} />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+          <Swiper
+            loop
+            slidesPerView={4}
+            spaceBetween={32}
+            style={{ margin: '92px 0', width: '100%' }}
+          >
+            {sponsors.map((sponsor, index) => (
+              <SwiperSlide key={index}>
+                <div key={index} className={styles.landingContact__swiperItem}>
+                  <img alt={sponsor.alt} src={sponsor.asset} className={styles.landingContact__swiperItem__image} />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
 
         <div className={styles.landingContact__overlay__contacts}>
-          <div className={styles.landingContact__overlay__contacts__col}>
-            <h1 className={styles.landingContact__overlay__contacts__col__title}>
-              Nous contacter
-            </h1>
-            <p className={styles.landingContact__overlay__contacts__col__description}>
-              Remplissez le formulaire et nous vous répondrons le plus rapidement possible.
-            </p>
+          <div className={styles.landingContact__overlay__contacts__inner}>
+            <div className={styles.landingContact__overlay__contacts__col}>
+              <h1 className={styles.landingContact__overlay__contacts__col__title}>
+                Nous contacter
+              </h1>
+              <p className={styles.landingContact__overlay__contacts__col__description}>
+                Remplissez le formulaire et nous vous répondrons le plus rapidement possible.
+              </p>
 
-            <div className={styles.landingContact__overlay__contacts__col__icons}>
-              <div className={styles.landingContact__overlay__contacts__col__icons__inner}>
+              <div className={styles.landingContact__overlay__contacts__col__icons}>
+                <div className={styles.landingContact__overlay__contacts__col__icons__inner}>
 
-                <button className={styles.landingContact__overlay__contacts__col__icons_btn}>
-                  <Icon style={{ fontSize: 24, color: '#0C75FF', margin: '2px 0 0 8px' }}>
-                    mail
-                  </Icon>
-                  <p className={styles.landingContact__overlay__contacts__col__icons_btn__title}>
-                    contact@asvf-montagne.fr
-                  </p>
-                </button>
-                <button className={styles.landingContact__overlay__contacts__col__icons_btn}>
-                  <Icon style={{ fontSize: 24, color: '#0C75FF', margin: '2px 0 0 8px' }}>
-                    room
-                  </Icon>
-                  <p className={styles.landingContact__overlay__contacts__col__icons_btn__title}>
-                    22 rue du Passou 38090 Vaulx-Milieu
-                  </p>
-                </button>
-                <button className={styles.landingContact__overlay__contacts__col__icons_btn}>
-                  <Icon style={{ fontSize: 24, color: '#0C75FF', margin: '2px 0 0 8px' }}>
-                    phone
-                  </Icon>
-                  <p className={styles.landingContact__overlay__contacts__col__icons_btn__title}>
-                    06 81 26 88 14 (jeudi de 18h30 à 20h)
-                  </p>
-                </button>
+                  <button className={styles.landingContact__overlay__contacts__col__icons_btn}>
+                    <Icon style={{ fontSize: 24, color: '#0C75FF', margin: '2px 0 0 8px' }}>
+                      mail
+                    </Icon>
+                    <p className={styles.landingContact__overlay__contacts__col__icons_btn__title}>
+                      contact@asvf-montagne.fr
+                    </p>
+                  </button>
+                  <button className={styles.landingContact__overlay__contacts__col__icons_btn}>
+                    <Icon style={{ fontSize: 24, color: '#0C75FF', margin: '2px 0 0 8px' }}>
+                      room
+                    </Icon>
+                    <p className={styles.landingContact__overlay__contacts__col__icons_btn__title}>
+                      22 rue du Passou 38090 Vaulx-Milieu
+                    </p>
+                  </button>
+                  <button className={styles.landingContact__overlay__contacts__col__icons_btn}>
+                    <Icon style={{ fontSize: 24, color: '#0C75FF', margin: '2px 0 0 8px' }}>
+                      phone
+                    </Icon>
+                    <p className={styles.landingContact__overlay__contacts__col__icons_btn__title}>
+                      06 81 26 88 14 (jeudi de 18h30 à 20h)
+                    </p>
+                  </button>
+                </div>
               </div>
+
+              {size <= 1184 && (
+                <LandingForm
+                  position="center"
+                  email={email}
+                  fullName={fullName}
+                  message={message}
+                  setEmail={setEmail}
+                  setFullName={setFullName}
+                  setMessage={setMessage}
+                  onSubmit={onSubmit}
+                />
+              )}
             </div>
 
-            {size <= 1184 && (
+            {size > 1184 && (
               <LandingForm
-                position="center"
                 email={email}
                 fullName={fullName}
                 message={message}
@@ -125,18 +141,6 @@ export default function LandingContact({ fullName, setFullName, email, setEmail,
               />
             )}
           </div>
-
-          {size > 1184 && (
-            <LandingForm
-              email={email}
-              fullName={fullName}
-              message={message}
-              setEmail={setEmail}
-              setFullName={setFullName}
-              setMessage={setMessage}
-              onSubmit={onSubmit}
-            />
-          )}
         </div>
       </div>
 
