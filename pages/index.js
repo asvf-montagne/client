776ab95634ca3509d2afc08bd5ed24099d4b1650
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-10-30 22:16:12 +0100

    improvement: add prefetch for stories on the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from "@components/atoms/Layout";
 import LandingInfo from "@components/organisms/LandingInfo";
 import LandingContact from "@components/organisms/LandingContact";
 import services from "../services";
+import { useRouter } from 'next/router';
 
 function Home({ stories }) {
+  const router = useRouter()
+
   const [fullName, setFullName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
@@ -13,6 +16,13 @@ function Home({ stories }) {
     event.preventDefault()
     console.log(fullName, email, message)
   }
+
+  useEffect(() => {
+
+    // In order to reduce amount time to load the stories page
+    router.prefetch('/stories').then(() => console.info('fetched stories'))
+
+  }, [])
 
   return (
     <Layout>
