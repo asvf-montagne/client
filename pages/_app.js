6e6ff0a2eaa116812f36bca143dc16b1b1bdbed5
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-11-01 15:50:59 +0100

    fix: scroll to top when changing of route

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,16 @@ import 'swiper/components/scrollbar/scrollbar.scss';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
+import useRouterScroll from "@hooks/useRouterScroll";
 
-//Binding events.
-Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 export default function Application({ Component, pageProps }) {
+  useRouterScroll();
+
   return (
     <Component {...pageProps} />
   );
