e98458e70074f23e6f2f18168238a203351fe7b0
antoineaudrain <antoine.audrain@outlook.fr>
2020-11-04 11:49:18 +0100

    add dynamic burger icon

--- components/atoms/Navigation.js
+++ components/atoms/Navigation.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Icon from '@material-ui/core/Icon';
 import Button from '@components/atoms/Button';
 import styles from './Navigation.module.css';
 
 export default function Navigation() {
   const router = useRouter();
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
   const handleRedirection = () => {
     router.push('/sign-in')
@@ -14,6 +17,7 @@ export default function Navigation() {
       <div className={styles.header__container}>
         <ul className={styles.header__list}>
           <a className={styles.header__logo} href="/">ASVF Montagne</a>
+          <a className={styles.header__logo__min} href="/">ASVF</a>
           <li className={router.pathname.includes('/club') ? styles.header__list__itemActive : styles.header__list__item}>
             <a className={styles.header__list__item__link} href="/club">Le Club</a>
           </li>
@@ -26,9 +30,16 @@ export default function Navigation() {
           <li className={router.pathname.includes('/sign-up') ? styles.header__list__itemActive : styles.header__list__item}>
             <a className={styles.header__list__item__link} href="/sign-up">Register</a>
           </li>
-          <Button onClick={handleRedirection} type="light" focus="light" style={{ marginLeft: 32 }}>
-            Connexion
-          </Button>
+          <li className={styles.header__list__item}>
+            <Button onClick={handleRedirection} type="light" focus="light">
+              Connexion
+            </Button>
+          </li>
+          <li className={styles.header__list__item} id="burger">
+            <Button onClick={() => setIsMenuActive(!isMenuActive)} type="link" focus="primary">
+              <Icon>{isMenuActive ? 'close' : 'menu'}</Icon>
+            </Button>
+          </li>
         </ul>
       </div>
     </nav>
