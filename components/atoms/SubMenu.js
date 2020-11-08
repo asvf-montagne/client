f607428416eac1d45e18309b122d08e4b1ce47ee
antoineaudrain <antoine.audrain@outlook.fr>
2020-11-08 18:31:25 +0100

    dynamic drop down content

--- components/atoms/SubMenu.js
+++ components/atoms/SubMenu.js
@@ -1,42 +1,78 @@
 import styles from './SubMenu.module.css';
 
+const items = [
+  {
+    title: 'nouveau',
+    links: [
+      {
+        label: 'Presentation du club',
+        url: '/'
+      },
+      {
+        label: "Ecole d'escalade",
+        url: '/'
+      },
+      {
+        label: "Le mur d'escalade",
+        url: '/'
+      },
+      {
+        label: 'Inscription au club',
+        url: '/'
+      }
+    ]
+  },
+  {
+    title: 'adhérent',
+    links: [
+      {
+        label: 'Prochaines sorties',
+        url: '/'
+      },
+      {
+        label: "reglement interieur",
+        url: '/'
+      },
+      {
+        label: "Location materiel",
+        url: '/'
+      }
+    ]
+  },
+  {
+    title: 'infos',
+    links: [
+      {
+        label: 'COVID-19',
+        url: '/'
+      },
+      {
+        label: "notices assurance",
+        url: '/'
+      },
+      {
+        label: "questionnaire santé",
+        url: '/'
+      }
+    ]
+  },
+]
+
 export default function SubMenu() {
   return (
     <div className={styles.submenu}>
-      <div className={styles.submenu_group}>
-        <h6 className={styles.submenu_group_title}>
-          nouveau ?
-        </h6>
-        <a className={styles.submenu_group_link} href="">
-          Présentation du club
-        </a>
-        <a className={styles.submenu_group_link} href="">
-          École d'éscalade
-        </a>
-        <a className={styles.submenu_group_link} href="">
-          Le mur d'éscalade
-        </a>
-        <a className={styles.submenu_group_link} href="">
-          Adhésion au club
-        </a>
-      </div>
-      <div className={styles.submenu_group}>
-        <h6 className={styles.submenu_group_title}>
-          adhérent
-        </h6>
-        <a className={styles.submenu_group_link} href="">
-          Prochaines sorties
-        </a>
-        <a className={styles.submenu_group_link} href="">
-          Réglement intérieur
-        </a>
-        <a className={styles.submenu_group_link} href="">
-          Location matériel
-        </a>
-        <a className={styles.submenu_group_link} href="">
-          COVID-19
-        </a>
-      </div>
+      {items.map((item) => (
+        <div className={styles.submenu_group}>
+          <h6 className={styles.submenu_group_title}>
+            {item.title}
+          </h6>
+          {item.links.map((link) => (
+            <a className={styles.submenu_group_link} href={link.url}>
+              {link.label}
+            </a>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
