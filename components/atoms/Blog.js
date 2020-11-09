4d66f2362ca93eb33450a2d65cac98ca1e917ba4
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-11-09 15:31:01 +0100

    feature(image-upload): fix club

--- components/atoms/Blog.js
+++ components/atoms/Blog.js
@@ -14,8 +14,6 @@ export default function Blog({ data, ...props }) {
   return (
     <section className={styles.blog} {...props}>
       <div className={styles.blog__inner}>
-        <UploadImageInput/>
-
         <EditorRendererProvider
           style={{
             header: {
