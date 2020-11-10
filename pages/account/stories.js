1187348c81082acef2f5a49bc89b2ee995ab7c88
antoineaudrain <antoine.audrain@outlook.fr>
2020-11-10 18:57:50 +0100

    account routes

--- pages/account/stories.js
+++ pages/account/stories.js
@@ -2,11 +2,8 @@ import React from 'react'
 import Layout from '@components/atoms/Layout'
 import SplitBackgroundOverlay from '@components/atoms/SplitBackgroundOverlay'
 import PageHeader from '@components/atoms/PageHeader'
-import Blog from '@components/atoms/Blog'
 
-import mockStories from '../mockStories'
-
-export default function Club() {
+export default function AccountStories() {
   return (
     <Layout>
       <SplitBackgroundOverlay padding="96px 0 180px 0" topHalfHeight={65}>
@@ -15,7 +12,6 @@ export default function Club() {
           subTitle="Dernière mise à jour le 18/03/2020"
         />
       </SplitBackgroundOverlay>
-      <Blog data={mockStories[0].data} />
     </Layout>
   )
 }
