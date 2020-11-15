a9ee1b6ad3f4522d9ab836c5430e53e85d8fdbbd
antoineaudrain <antoine.audrain@outlook.fr>
2020-11-15 12:30:30 +0100

    start refacto navigation

--- components/molecules/Navigation.js
+++ components/molecules/Navigation.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Icon from '@material-ui/core/Icon'
 import PropTypes from 'prop-types'
