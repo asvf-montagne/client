69339a417ad3379becce7fb7ed8007c57a0ee45b
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-12-04 11:53:15 +0100

    seo: hotfix sitemaps

--- utils/SeoGeneratorPlugin.js
+++ utils/SeoGeneratorPlugin.js
@@ -34,8 +34,8 @@ module.exports = class SeoGeneratorPlugin {
       pages.push(frontendURL + '/stories/' + id),
     )
 
-    const sitemap = `
-<?xml version="1.0" encoding="UTF-8"?>
+    const sitemap =
+`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages
   .sort()
