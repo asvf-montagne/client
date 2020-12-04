80afeb7969f7e76ad41725cdf56bcc860e2364f6
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-12-04 11:44:42 +0100

    seo: fix lint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const withImages = require('next-images')
 const SeoGeneratorPlugin = require('./utils/SeoGeneratorPlugin')
-const util = require("util")
+const util = require('util')
 
 module.exports = withImages({
   webpack(config, options) {
-
     if (options.isServer) {
       config.plugins.push(new SeoGeneratorPlugin())
     }
--- utils/SeoGeneratorPlugin.js
+++ utils/SeoGeneratorPlugin.js
@@ -1,62 +1,73 @@
 const fs = require('fs')
 const globby = require('globby')
-const fetch = require('node-fetch');
-
+const fetch = require('node-fetch')
 
 const backendURL = `https://backend.asvf-montagne.fr`
 const frontendURL = `https://beta.asvf-montagne.fr`
-
 
 async function getPostsIds() {
   return (await fetch(`${backendURL}/posts/view/ids?limit=1000`)).json()
 }
 
-
 module.exports = class SeoGeneratorPlugin {
   async generateSiteMap() {
-
-    const pages = (await globby([
-      'pages/**/*.js',
-      '!pages/_*.js',
-      '!pages/**/[id].js',
-      '!pages/api',
-      '!pages/dashboard/**',
-    ])).map(page => frontendURL + page.replace('pages', '')
-      .replace('.js', '')
-      .replace('.md', '')
-      .replace('/index', '')
-    );
-
+    const pages = (
+      await globby([
+        'pages/**/*.js',
+        '!pages/_*.js',
+        '!pages/**/[id].js',
+        '!pages/api',
+        '!pages/dashboard/**',
+      ])
+    ).map(
+      (page) =>
+        frontendURL +
+        page
+          .replace('pages', '')
+          .replace('.js', '')
+          .replace('.md', '')
+          .replace('/index', ''),
+    )
 
     // add stories to pages
-    (await getPostsIds()).forEach(id => pages.push(frontendURL + '/stories/' + id))
+    ;(await getPostsIds()).forEach((id) =>
+      pages.push(frontendURL + '/stories/' + id),
+    )
 
     const sitemap = `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.sort().map(page => `
+${pages
+  .sort()
+  .map(
+    (page) => `
   <url>
       <loc>${page}</loc>
   </url>
-`).join('')}
+`,
+  )
+  .join('')}
 </urlset>`
 
     fs.writeFileSync('public/sitemap.xml', sitemap)
   }
 
   generateRobot() {
-    fs.writeFileSync('public/robots.txt', `
+    fs.writeFileSync(
+      'public/robots.txt',
+      `
 User-agent: *
 Allow: /
 Disallow: /dashboard/
 
-Sitemap: ${frontendURL}/sitemap.xml`)
+Sitemap: ${frontendURL}/sitemap.xml`,
+    )
   }
 
   apply(compiler) {
     compiler.hooks.emit.tapPromise('SeoGeneratorPlugin', async () => {
       await this.generateSiteMap()
       this.generateRobot()
-    });
+    })
   }
 }
